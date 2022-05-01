29fd9962b01bed97149e0b0541960ea5a16cc112
Tommy Gorham <38857089+tommygorham@users.noreply.github.com>
2022-05-01 05:18:01 -0400

    Add supplemental notes
    Also fix missing scope bracket

--- useful_util/learningteams.cpp
+++ useful_util/learningteams.cpp
@@ -1,3 +1,10 @@
+// I wrote this program to help myself understand how to control and understand Kokkos Team Policies. 
+// You can only call these member variables inside of parallel regions, so its a bit more challenging 
+// if you are coming from openmp where you can say something like (e.g., omp_get_num_processors()) 
+// anywhere in the program. BTW, if you compiled for OpenMP, you can omp_get_num_processors() normally 
+// nut the functions implemented below require the parallel regon to query. Hopefully it will help you
+// like it helped me! 
+
 #include <Kokkos_Core.hpp>
 #include <iostream> 
 
@@ -5,10 +12,12 @@ int main()
 {
 
       Kokkos::initialize(); 
-      
+      { /*start kokkos scope*/ 
       typedef Kokkos::TeamPolicy<>::member_type member_type; 
       Kokkos::TeamPolicy<> policy(2, Kokkos::AUTO() ); //2 teams, let kokkos decide num threads per team 
-
+      
+      /*launch parallel region using our policy we created on line 17 
+        query member vars with team_member*/ 
       Kokkos::parallel_for(policy, KOKKOS_LAMBDA (member_type team_member)
       { 
           int threadID = team_member.team_rank(); /*what thread am i on this team*/
@@ -20,9 +29,7 @@ int main()
           std::cout << "\nNumber of Teams: " << num_teams;
           std::cout << "\nThreads/team: " << threads_per_team; 
      }); 
-      
-      
+     
       Kokkos::finalize();
-      }
-      
-      }
+      } /* close kokkos scope here*/      
+}
