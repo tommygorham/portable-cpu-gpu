3169be57151d6e161043c350b683fa2f905849fd
Tommy Gorham <38857089+tommygorham@users.noreply.github.com>
2022-05-02 02:10:15 -0400

    Formatting / Spacing
    Easier to read now. Need a vim plug-in for this.

--- Program4/finaltest.cpp
+++ Program4/finaltest.cpp
@@ -1,5 +1,5 @@
 // Program Description: This Program Sorts a 1-Dimensional Datatype on the CPU or the GPU
-// Using Kokkos Parallel Sort. 
+// Using Kokkos Parallel Sort and serial merge sort. 
 // Purpose: To demonstrate how to use Kokkos for performance portability, to then 
 // compile for and test with different backends enabled (Serial, OpenMP, Cuda, All 3, etc..)  
 // Author: Thomas Gorham
@@ -19,59 +19,50 @@ void merge_sort(LinearType arr, int low, int high);
 
 int main()
 {
-
-      Kokkos::initialize();
-      {
-		 
-	       int global_m = 1024;   /*size of view to sort -- can hard set or change to atoi[1] */ 	
-		   /* init view with random values\*/ 
-           LinearType A("A", global_m); /*for test 1*/ 
+	Kokkos::initialize();
+        {
+		/*hard set for now*/ 
+		int global_m = 100000;   /*size of view to sort -- can hard set or change to atoi[1] */ 	
+		/* init view with random values\*/ 
+                 LinearType A("A", global_m); /*for test 1*/ 
 		// LinearType B("B", global_m); /*for test 2*/ 
-           LinearType::HostMirror h_A = Kokkos::create_mirror_view(A); 
-        // LinearType::HostMirror h_B = Kokkos::create_mirror_view(B); 
+                 LinearType::HostMirror h_A = Kokkos::create_mirror_view(A); 
+                 // LinearType::HostMirror h_B = Kokkos::create_mirror_view(B); 
 		  
-		 
-          for(int i = 0; i < global_m; ++i){
-              h_A(i) = rand() % 100; 
-          }
-        
-        /*
-         for(int i = 0; i < global_m; ++i){
-              h_B(i) = rand() % 100; 
-          }
-        */
-		   /*print if size small*/ 
-		   if (global_m < 20) {
-		    std::cout << "\n\nUnsorted Arrays:\n";
-			printVector(h_A); /* printing on host always */ 
-		//	printVector(h_arr2);
-		   }
-		    Kokkos::deep_copy(A, h_A); 	
-            auto start = std::chrono::high_resolution_clock::now();
-		    Kokkos::sort(A, 0, global_m);
-			auto end = std::chrono::high_resolution_clock::now(); 
-			std::chrono::duration<double, std::milli> mstime = end - start; 
-            std::cout << "\nKokkos Sort Took: " << mstime.count() << " milliseconds"; 
-            //std::chrono::duration<double, std::micro> microtime = end - start;
-            Kokkos::deep_copy(h_A, A); 
-			
-			/*Test 2*/ 
-		/*	auto start2 = std::chrono::high_resolution_clock::now();
-			merge_sort(arr2, 0, global_m-1);
-			auto end2 = std::chrono::high_resolution_clock::now(); 
-			std::chrono::duration<double, std::milli> mstime2 = end2 - start2; 
-            std::cout << "\nMerge Sort Took: " << mstime2.count() << " milliseconds"; 
-			Kokkos::deep_copy(h_arr2, arr2);
-		*/	
-			/* ensure accurate sorting */ 
-		    if(global_m < 20) {
-			std::cout << "\n\nSorted Arrays:\n";
-            printVector(h_A);
-		//	printVector(h_arr2); 
+                 for(int i = 0; i < global_m; ++i){ h_A(i) = rand() % 100;} /* fill with unsorted values */ 
+                 /* for addittional testing
+                 for(int i = 0; i < global_m; ++i){ h_B(i) = rand() % 100;}
+                 */
+		 /*print if size small*/ 
+		 if (global_m < 20) {
+		 std::cout << "\n\nBefore sorting:\n";
+	         printVector(h_A); /* printing on host always */ 
+		 }
+		 Kokkos::deep_copy(A, h_A); 	
+                 auto start = std::chrono::high_resolution_clock::now();
+		 Kokkos::sort(A, 0, global_m);
+	         auto end = std::chrono::high_resolution_clock::now(); 
+		 std::chrono::duration<double, std::milli> mstime = end - start; 
+                 std::cout << "\nKokkos Sort Took: " << mstime.count() << " milliseconds"; 
+                 //std::chrono::duration<double, std::micro> microtime = end - start;
+                 Kokkos::deep_copy(h_A, A); 
+	         /* for test 2 merge sort 
+		 auto start2 = std::chrono::high_resolution_clock::now();
+	         merge_sort(arr2, 0, global_m-1);
+	         auto end2 = std::chrono::high_resolution_clock::now(); 
+	         std::chrono::duration<double, std::milli> mstime2 = end2 - start2; 
+                 std::cout << "\nMerge Sort Took: " << mstime2.count() << " milliseconds"; 
+	         Kokkos::deep_copy(h_arr2, arr2);
+		 */	
+	         /* check for accurate sorting */ 
+		 if(global_m < 20) {
+			 std::cout << "\n\nSorted Arrays:\n";
+                         printVector(h_A);
+		       //printVector(h_arr2); 
 		    }
-	  }
+	  } /*close kokkos scope*/ 
 	  Kokkos::finalize();  
-}
+} /*end main*/ 
 
 
 void printVector(LinearType::HostMirror vec)
